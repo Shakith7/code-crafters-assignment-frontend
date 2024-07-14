737f58b228e742720b089dd9a40519b7c0c96e01
Shakith7 <mindula.20200697@iit.ac.lk>
2024-07-14 13:14:05 +0530

    asfdwerf

--- src/components/Breadcrumb.js
+++ src/components/Breadcrumb.js
@@ -8,21 +8,21 @@ const Breadcrumb = () => {
   const pathnames = location.pathname.split('/').filter((x) => x);
 
   return (
-    <nav className="flex justify-between items-center py-3 px-5  bg-gray-50 rounded-lg border border-gray-200 text-[#764EE8] " aria-label="Breadcrumb">
-      <ol className="inline-flex items-center space-x-1 md:space-x-3 text-[#764EE8]">
+    <nav className="flex justify-between items-center py-3 px-5  bg-gray-50 rounded-lg border border-gray-200  " aria-label="Breadcrumb">
+      <ol className="inline-flex items-center space-x-1 md:space-x-3  ">
         {pathnames.map((name, index) => {
           const routeTo = `/${pathnames.slice(0, index + 1).join('/')}`;
           const isLast = index === pathnames.length - 1;
           return (
             <li key={name}>
-              <div className="flex items-center text-[#764EE8] font-semibold">
+              <div className="flex items-center  font-semibold">
                 <svg className="w-6 h-6 text-[#764EE8]" fill="#764EE8" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd"></path></svg>
                 {!isLast ? (
-                  <Link to={routeTo} className="ml-1 text-base font-medium text-[#764EE8] hover:text-blue-600 md:ml-2 font-semibold ">
+                  <Link to={routeTo} className="ml-1 text-base font-medium text-gray-500 hover:text-blue-600 md:ml-2 font-semibold ">
                     {name.charAt(0).toUpperCase() + name.slice(1)}
                   </Link>
                 ) : (
-                  <span className="ml-1 text-base font-medium  md:ml-2 text-[#764EE8] font-semibold ">
+                  <span className="ml-1 text-base font-medium  md:ml-2  font-semibold text-[#764EE8]">
                     {name.charAt(0).toUpperCase() + name.slice(1)}
                   </span>
                 )}
